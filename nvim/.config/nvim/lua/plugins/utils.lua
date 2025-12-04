cb27fc736aca1422cc73bb0cebeddabf56ac115c
Anish Nyayachavadi <anishnya@icloud.com>
2025-12-03 22:50:44 -0800

    Improvements!

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -26,6 +26,24 @@ local oil = {
                         { "name", "asc" },
                     },
                 },
+                keymaps = {
+                    ["g?"] = { "actions.show_help", mode = "n" },
+                    ["<CR>"] = "actions.select",
+                    ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+                    ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+                    ["<C-t>"] = { "actions.select", opts = { tab = true } },
+                    ["<C-p>"] = "actions.preview",
+                    ["<C-q>"] = { "actions.close", mode = "n" },
+                    ["<C-r>"] = "actions.refresh",
+                    ["-"] = { "actions.parent", mode = "n" },
+                    ["_"] = { "actions.open_cwd", mode = "n" },
+                    ["`"] = { "actions.cd", mode = "n" },
+                    ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+                    ["gs"] = { "actions.change_sort", mode = "n" },
+                    ["gx"] = "actions.open_external",
+                    ["g."] = { "actions.toggle_hidden", mode = "n" },
+                    ["g\\"] = { "actions.toggle_trash", mode = "n" },
+                },
             })
         end,
     }
@@ -230,7 +248,8 @@ local surround = {
 
 ---@type lz.n.pack.Spec
 local plugin_view = {
-    src = "https://github.com/adriankarlen/plugin-view.nvim",
+    -- "file:/// "
+    src = "https://github.com/anishnya/plugin-view.nvim.git",
     data = {
         "plugin-view.nvim",
         keys = {
@@ -279,8 +298,8 @@ local todo = {
     data = {
         "todo-comments.nvim",
         keys = {
-            { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
-            { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+            { "<leader>st", function() Snacks.picker.todo_comments() end,                                          desc = "Todo" },
+            { "<leader>sT", function() Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
         },
         after = function()
             require("todo-comments").setup()
